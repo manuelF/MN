9929980fe335b9c10cf1eff6718a1e118dd0259a
Manuel Ferreria <m.ferreria@gmail.com>
2013-04-01 10:40:28 -0300

    cambiado a un modulo para que llame a todo

--- tp1/matlab/histogramaYAjuste.m
+++ tp1/matlab/histogramaYAjuste.m
@@ -1,10 +1,31 @@
 #!/usr/local/bin/octave -qf
-source("leer_datos.m")
-source("dibujarHistyAjuste.m")
-source("GGDpdf_c.m")
+source("leer_datos.m");
+source("dibujarHistyAjuste.m");
+source("dibujarHist.m");
+source("GGDpdf_c.m");
 args = argv();
 d = leer_datos(args{1});
-dibujarHistyAjuste(d, str2double(args{2}),
+
+figure;
+hold on
+disp(args);
+
+dibujarHist(d);
+dibujarAjuste(d, str2double(args{2}),
                       str2double(args{3}),
-                      str2double(args{4}))
-print -dpng plot.png
+                      str2double(args{4}),
+                      'b');
+if(size(args,1)==8)
+dibujarAjuste(d, str2double(args{5}),
+                      str2double(args{6}),
+                      str2double(args{7}),
+                      'r');
+
+
+	fname =args{8};
+	print(fname, '-dpng');
+else
+
+	#print -dpng plot.png
+
+end
