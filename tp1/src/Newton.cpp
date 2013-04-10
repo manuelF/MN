a5cad7f85de6893c35f12de1a85393402f9512ad
Leopoldo Taravilse Diez <ltaravilse@ws9.labo7>
2013-04-10 15:33:14 -0300

    Toque regulaFalsi

--- tp1/src/Newton.cpp
+++ tp1/src/Newton.cpp
@@ -283,11 +283,24 @@ TFloat regulaFalsi(TFloat beta, TFloat beta2, int& iteraciones)
     TFloat beta3;
     TFloat fbeta=f(beta);
     TFloat fbeta2=f(beta2);
-    if (fbeta.dbl()*fbeta2.dbl()>0)
+    /*while (fbeta.dbl()*fbeta2.dbl()>0)
     {
-        cerr << setprecision(15);
-        cerr<< "==================fruta follows===================\n";
-        cerr<< beta.dbl() << ": " << fbeta.dbl() << "   "<<beta2.dbl() << ": " << fbeta2.dbl() << endl;
+	beta = beta + 1;
+	fbeta = f(beta);
+//        cerr << setprecision(15);
+//        cerr<< "==================fruta follows===================\n";
+//        cerr<< beta.dbl() << ": " << fbeta.dbl() << "   "<<beta2.dbl() << ": " << fbeta2.dbl() << endl;
+    }*/
+//Suponemos beta < beta2
+    while(fbeta.dbl()>0)
+    {
+	beta = beta/2.;
+	fbeta = f(beta);
+    }
+    while(fbeta2.dbl()<0)
+    {
+    	beta2 = beta2*2;
+	fbeta2 = f(beta2);
     }
     while(iteraciones<maximoIteraciones && fabs(beta.dbl()-beta2.dbl())>epsilon)
     {
