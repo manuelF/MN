7a889b017a4256b27a47fa2dcb36c8b4f13cd9f2
Manuel Ferreria <m.ferreria@gmail.com>
2013-04-14 16:26:35 -0300

    Agregada forma de tirar graficos de eps/iteraciones

--- tp1/matlab/generateAprox.sh
+++ tp1/matlab/generateAprox.sh
@@ -1,10 +1,22 @@
 #! /bin/bash
 
-
+paramfile=""
+            if [ $# -eq 0 ]
+            then
 paramfile="../informe/plots/parameters.txt"
+            else
+paramfile="../informe/plots/parameters-eps.txt"
+            fi
+
+
 
 files=(../datos/x_15_9_3.txt ../datos/x_2_62_35.txt ../datos/X1.txt ../datos/X2.txt ../datos/X3.txt ../datos/X4.txt ../datos/X5.txt ../datos/X6.txt ../datos/X7.txt)
-variables=(Beta Lambda Sigma)
+if [ $# -eq 0 ] 
+then
+    variables=(Beta Lambda Sigma)
+else
+    variables=(Beta Iteraciones)
+fi
 
 betas=(9 6.2)
 lambdas=(3 3.5)
@@ -14,10 +26,11 @@ index=0
 caso=1
 
 metodos=(Newton RegulaFalsi)
+nombreposta=(Newton Illinois)
 metval=(0 1)
 
 precision=(15 16 17 18 19 20 21 22 23 24 25 26 27)
-
+epsilons=(0.01 0.001 0.0001 0.00001 0.000001 0.0000001 0.00000001 0.000000001)
 for file in "${files[@]}"; do
     for met in "${metodos[@]}"; do
 
@@ -27,19 +40,37 @@ for file in "${files[@]}"; do
             a=$(grep "caso$caso" "$paramfile"| grep $met | grep "$var" | awk '{print $5}' | xargs)
             val=. read -a serie <<< "$a"
             index=0
+            if [ $# -eq 0 ]
+            then
             for prec in "${precision[@]}"; do
                 params+=" ${serie[$index]} "
                 index=$[$index +1]
             done
-
-            #filename="Caso$caso-$var"
-            filename="Caso$caso-$met"
+            else
+                for prec in "${epsilons[@]}"; do
+                params+=" ${serie[$index]} "
+                index=$[$index +1]
+            done
+ 
+            fi
+            if [ $# -eq 0 ]
+            then
+                filename="Caso$caso-$met"
+            else
+                filename="Caso$caso-$met-eps"
+            fi
 
 
         done
-        echo "./plotCurve.m ${precision[0]} $params $filename"
-        ./plotCurve.m ${precision[0]} $params $filename
-        mv "$filename" ../informe/plots/$filename.png
+            if [ $# -eq 0 ]
+            then
+                echo "./plotCurve.m ${precision[0]} $params $filename"
+               ./plotCurve.m ${precision[0]} $params $filename
+            else
+                 echo "./plotCurveSoloBeta.m 2 $params $filename"
+               ./plotCurveSoloBeta.m 2 $params $filename
+            fi
+        mv "$filename.png" ../informe/plots/$filename.png
     done
 	caso=$[$caso +1]
 done
