fc2aa4390e83d8568f28a32586b9b529a2baefbe
Manuel Ferreria <m.ferreria@gmail.com>
2013-05-05 15:52:37 -0300

    Agregado apendice A

--- tp2/src/DCT.cpp
+++ tp2/src/DCT.cpp
@@ -11,6 +11,8 @@
 
 #include <math.h>
 
+#define EPS 1e-4
+
 using namespace std;
 
 typedef vector<double> vd;
@@ -46,8 +48,64 @@ double psnr(const vd orig, const vd recup)
 {
     return 10 * log10((max_m()*max_m())/ecm(orig, recup));
 }
+vector<double> frecuencias;
+
+vector<double> muestreo;
+
+vector<double> lecturas;
+
+vector<vector<double> > M;
+
+void generarMatrizDCT(int n)
+{
+    frecuencias = vector<double> (n);
+    muestreo = vector<double> (n);
+    M=vector<vector<double> >(n);
+    
+    for(int i = 0; i< n; i++)
+        M[i]=vector<double>(n);
+
+    double k = (M_PI/(double)n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> lecturas[i];        
+        frecuencias[i]=i;
+        muestreo[i]=k*(1.0+((i/2)));
+    }
+#pragma omp parallel for
+    for(int i = 0; i< n ; i++)
+    {
+        #pragma omp parallel for
+        for(int j = 0; j< n ; j++)
+        {
+            M[i][j]=cos(frecuencias[i]*muestreo[j]);
+        }
+    }
+
+    double calc=0.0;
+    for(int i=0;i<n;i++) 
+        for(int j=0; j<n;j++)
+            calc+=M[i][j];
+    double constante=256.0;
+    cerr << calc<<endl;
+    if(abs(calc-constante)>EPS)
+        cerr << "DIFIEREN!" << endl;
+
+}
+
 
 int main(int argc, char* argv[])
 {
+
+    int n;
+
+    cin >> n;
+    lecturas = vector<double> (n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> lecturas[i];        
+    }
+    generarMatrizDCT(n);
+
     return 0;
 }
