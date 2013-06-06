74f71eb4b88a411afdc2836e3c043af7c8a1ea91
Manuel Ferreria <m.ferreria@gmail.com>
2013-06-06 12:05:09 -0300

    Agregada las imagenes que tenemos que reconocer

---
+++ tp3/datos/leerMNISTimage.m
@@ -0,0 +1,34 @@
+function img = leerMNISTimage(fname)
+% All the integers in the files are stored in the MSB first (high/big endian) 
+% TEST SET IMAGE FILE (t10k-images-idx3-ubyte):
+% 
+% [offset] [type]          [value]          [description] 
+% 0000     32 bit integer  0x00000803(2051) magic number 
+% 0004     32 bit integer  60000            number of images 
+% 0008     32 bit integer  28               number of rows 
+% 0012     32 bit integer  28               number of columns 
+% 0016     unsigned byte   ??               pixel 
+% 0017     unsigned byte   ??               pixel 
+% ........ 
+% xxxx     unsigned byte   ??               pixel
+% Pixels are organized row-wise. Pixel values are 0 to 255. 0 means background, 255 means foreground. 
+
+
+  fp = fopen(fname);
+  if fp == -1
+   error(['Error: no se puede abrir archivo ' fname]);
+  end	
+  
+  magicnumber = fread(fp,1,'uint32','ieee-be');
+  nimgs = fread(fp,1,'uint32','ieee-be');
+  nrows = fread(fp,1,'uint32','ieee-be');
+  ncols = fread(fp,1,'uint32','ieee-be');
+  
+  img = zeros(nrows,ncols,nimgs,'uint8');
+  
+  for imind=1:nimgs,
+     im = fread(fp,[ncols,nrows],'*uint8');
+     img(:,:,imind) = im';
+  end
+  
+  fclose(fp);
---
+++ tp3/datos/leerMNISTlabel.m
@@ -0,0 +1,21 @@
+function labels = leerMNISTlabel(fname)
+% All the integers in the files are stored in the MSB first (high/big endian) 
+% [offset] [type]          [value]          [description] 
+% 0000     32 bit integer  0x00000801(2049) magic number (MSB first) 
+% 0004     32 bit integer  60000            number of items 
+% 0008     unsigned byte   ??               label 
+% 0009     unsigned byte   ??               label 
+% ........ 
+% xxxx     unsigned byte   ??               label
+% The labels values are 0 to 9.
+
+
+  fp = fopen(fname,'r');
+  if fp == -1
+   error(['Error: no se puede abrir archivo ' fname]);
+  end	
+  
+  magicnumber = fread(fp,1,'uint32','ieee-be');
+  nitems = fread(fp,1,'uint32','ieee-be');
+  
+  labels = fread(fp,nitems,'*uint8');
