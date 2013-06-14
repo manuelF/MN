23e653fed954013dc2548ed1168d57fe9e6fc965
Manuel Ferreria <m.ferreria@gmail.com>
2013-06-14 10:47:17 -0300

    calculo dematriz de covarianza

--- tp3/datos/tp.m
+++ tp3/datos/tp.m
@@ -1,16 +1,15 @@
 
 % Haciendo el TP
 from=1;
-limit=2000; %cuantas imagenes maximo leemos
+limit=6000; %cuantas imagenes maximo leemos
 width=784;
 
-imggg=double(leerMNISTimage('Training Images',from,limit)); 
-labels=leerMNISTlabel('Training Labels',from,limit);
-hold on
-allmeans=zeros(10,width);
-for nrs=0:9,
+if ~(exist('covarianza.mat','file')==2)
 
-    [imgg, nl] =filterimages(imggg,labels,[1]*nrs); %deja solo las iamgenes con label []
+    imgg=double(leerMNISTimage('Training Images',from,limit)); 
+    labels=leerMNISTlabel('Training Labels',from,limit);
+    allmeans=zeros(10,width);
+
 
     nrows = size(imgg,1); %cuantas imagenes quedaron
 
@@ -23,23 +22,19 @@ for nrs=0:9,
         M(im,:)=(X(im)-mu); %obtenemos la varianza de cada imagen
     end
     M=M/sqrt(width-1); %dividimos por n-1
-    
+
     [U,S,V]=svd(M); %sacamos la svd de esa
-
-    % V = avec(M*M') covarianza
-
-    xp=zeros(nrows ,width); %otra matriz temporal
-    for im=1:nrows , 
-        xp(im,:)=V'*X(im,:)'; %aplicamos la transformacion a cada elemento
-    end
-    pointy=mean(xp);
-    allmeans(nrs+1,:)=pointy(:);
-    scatter3(pointy(:,1),pointy(:,2),pointy(:,3),15,nl(1));
-    %scatter3(xp(:,1),xp(:,2),xp(:,3),3,nl) %ploteamos tres coordenadas
+    save('covarianza.mat','V');
 end
-from=1;
-limit = 10;
+load('covarianza.mat','V');
+% V = avec(M*M') covarianza
 
-testImg=double(leerMNISTimage('Training Images',from,limit)); 
-testLabels=leerMNISTlabel('Training Labels',from,limit);
-%xpp=[xp,nl];
+xp=zeros(nrows ,width); %otra matriz temporal
+for im=1:nrows , 
+    xp(im,:)=V'*X(im,:)'; %aplicamos la transformacion a cada elemento
+end
+pointy=mean(xp);
+allmeans(nrs+1,:)=pointy(:);
+%scatter3(pointy(:,1),pointy(:,2),pointy(:,3),15,nl(1));
+scatter3(xp(:,1),xp(:,2),xp(:,3),3,nl) %ploteamos tres coordenadas
+
