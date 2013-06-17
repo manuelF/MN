04063b61f6f05b0cfc24f52581832847c1796a97
Leopoldo Taravilse <ltaravilse@gmail.com>
2013-06-17 18:11:28 -0300

    Householder en n3 andando

--- tp3/src/OCR.cpp
+++ tp3/src/OCR.cpp
@@ -34,11 +34,9 @@ vector<vector<double> > mult(vector<vector<double> > &A, vector<vector<double> >
 	int t = A[0].size(); // tiene que ser igual a B.size();
     vector<vector<double> > B2 = B;
     transpose(B2);
-	cerr << n <<" "<< m <<" "<<t<<endl;
 	vector<vector<double> > res(n,vector<double>(m,0));
 	for(int i=0;i<n;i++)
 	{
-        #pragma omp parallel for
     	for(int j=0;j<m;j++)
         {
             double sum =0.0;
@@ -49,7 +47,6 @@ vector<vector<double> > mult(vector<vector<double> > &A, vector<vector<double> >
             res[i][j]=sum;
         }
     }
-    cerr <<"Termino mult"<< endl;
 	return res;
 }
 
@@ -77,10 +74,6 @@ void generateMx()
 	Xt = X;
 	transpose(Xt);
 	Mx = mult(Xt,X);
-/*	int n = Mx.size();
-	for(int i=0;i<n;i++)
-	for(int j=0;j<n;j++)
-		Mx[i][j] /= (n-1);*/
 }
 
 double norm(vector<double> &vec)
@@ -91,52 +84,69 @@ double norm(vector<double> &vec)
 	return sqrt(res);
 }
 
-vector<vector<double> > aux;
+vector<vector<double> > Id(int n)
+{
+    vector<vector<double> > A(n,vector<double>(n,0));
+    for(int i = 0; i< n; i++)
+        A[i][i]=1;
+    return A;
+}
 
-void householder(vector<vector<double> > &A, vector<vector<double> > &Q, vector<vector<double> > &R)
+vector<double> u,v;
+vector<vector<double> > Q,R,aux,aux2;
+
+void householder(vector<vector<double> > &A)
 {
 	int n = A.size();
-	Q.clear();
-	Q.resize(n,vector<double>(n,0));
-	for(int i=0;i<n;i++)
-		Q[i][i] = 1;
 	aux.clear();
+	aux2.clear();
 	aux.resize(n,vector<double>(n));
+	aux2.resize(n,vector<double>(n));
 	R = A;
-	for(int i=0;i<n;i++)
+	Q = Id(n);
+	for(int i=0;i<n-1;i++)
 	{
-		vector<double> v;
-		for(int j=i;j<n;j++)
-			v.push_back(R[j][i]);
-		double alpha = norm(v);
-		v[0] = alpha;
-		for(int j=1;j<(int)v.size();j++)
-			v[j] = 0;
-		for(int j=0;j<(int)v.size();j++)
-			v[j] = R[j+i][i] - v[j];
-		alpha = norm(v);
-		for(int j=0;j<(int)v.size();j++)
-			v[j] = v[j]/alpha;
-		for(int a=0;a<n;a++)
-        {
-            for(int b=0;b<n;b++)
-            {
-                if(a<i||b<i)
-                {
-                    if(a==b)
-                        aux[a][b] = 1.;
-                    else
-                        aux[a][b] = 0.;
-                }
-                else if(a==b)
-                    aux[a][b] = 1.-2.*v[a-i]*v[b-i];
-                else
-                    aux[a][b] = -2.*v[a-i]*v[b-i];
-            }
-        }
-		Q = mult(Q,aux);
-		transpose(aux);
-		R = mult(aux,R);
+	    u.clear();
+	    for(int j=0;j<i;j++)
+            u.push_back(0);
+	    for(int j=i;j<n;j++)
+            u.push_back(R[j][i]);
+        double alpha = norm(u);
+        if(abs(alpha) < 1e-6)
+            continue;
+        if(alpha*u[i]>0)
+            alpha *= -1.;
+        u[i] += alpha;
+        alpha = norm(u);
+        v.clear();
+        for(int j=0;j<n;j++)
+            v.push_back(u[j]/alpha);
+        /** Inicio calculo R **/
+        for(int j=0;j<n;j++)
+            u[j] = 0;
+        for(int t=0;t<n;t++)
+        for(int j=0;j<n;j++)
+            u[j] += v[t]*R[t][j];
+        for(int j=0;j<n;j++)
+        for(int t=0;t<n;t++)
+            aux[j][t] = 2*v[j]*u[t];
+        for(int j=0;j<n;j++)
+        for(int t=0;t<n;t++)
+            R[j][t] -= aux[j][t];
+        /** Fin calculo R **/
+        /** Inicio calculo Q **/
+        for(int j=0;j<n;j++)
+            u[j] = 0;
+        for(int t=0;t<n;t++)
+        for(int j=0;j<n;j++)
+            u[j] += Q[j][t]*v[t];
+        for(int j=0;j<n;j++)
+        for(int t=0;t<n;t++)
+            aux2[j][t] = 2*u[j]*v[t];
+        for(int j=0;j<n;j++)
+        for(int t=0;t<n;t++)
+            Q[j][t] -= aux2[j][t];
+        /** Fin calculo Q **/
 	}
 	return;
 }
@@ -150,15 +160,8 @@ bool sigoIterando(vector<vector<double> > &A)
 	for(int i=0;i<n;i++)
 	    for(int j=0;j<i;j++)
 		    res += abs(A[i][j]);
+    cerr<<"Res: "<< res << endl;
 	return res>delta;
-}
-
-vector<vector<double> > Id(int n)
-{
-    vector<vector<double> > A(n,vector<double>(n,0));
-    for(int i = 0; i< n; i++)
-        A[i][i]=1;
-    return A;
 }
 
 bool sim(const vector<vector<double> > & A)
@@ -179,13 +182,12 @@ bool sim(const vector<vector<double> > & A)
 
 void eig(vector<vector<double> > &A, vector<vector<double> > &auVec)
 {
-	vector<vector<double> > Q,R;
 	int n = A.size(); /// A es cuadrada
 	auVec = Id(n);
 	int iter = 0;
 	while(sigoIterando(A))
 	{
-		householder(A,Q,R);
+		householder(A);
 		A = mult(R,Q);
 		auVec = mult(auVec,Q);
 	}
@@ -277,7 +279,6 @@ int main()
     generateX();
     cerr << 1 << endl;
     generateMx();
-    cerr<<2 << endl;
     eig(Mx,av);
     cerr << 3 << endl;
     fillTC(k);
