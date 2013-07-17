9a4b58fd5557ef905533ba4beeb7cd20f8c6b7e9
Manuel Ferreria <m.ferreria@gmail.com>
2013-07-17 10:31:41 -0300

    asdasd

--- tp3/qr_exps/HR_its.m
+++ tp3/qr_exps/HR_its.m
@@ -1,4 +1,6 @@
-iteraciones=200
+iteraciones=200;
+load('HRs.mat');
+
 h=figure;
 plot([smooth(HR_100_0(1:iteraciones,1)),smooth(HR_100_0(1:iteraciones,2)),smooth(HR_100_0(1:iteraciones,3))]);
 title('Hitrate a 500 imgs con 100 componentes, en funcion de iteraciones QR','FontSize',14.0);
@@ -40,5 +42,27 @@ ylabel('Hitrate de deteccion','FontSize',14.0);
 legend('5k VecinosCercanos','15k VecinosCercanos','30k VecinosCercanos','Location','SouthEast');
 axis([1 iteraciones 0.0 1]);
 filename=sprintf('../informe/plots/HR_10_0');
+saveas(h,filename,'png')
+saveas(h,filename,'fig')
+
+h=figure;
+semilogy([SUM(:,1), SUM(:,2), SUM(:,3)]);
+title('Suma elementos bajo la diagonal, en funcion de las iteraciones','FontSize',14.0);
+legend('Suma 5k imagenes','Suma 15k imagenes','Suma 30k imagenes','Location','NorthEast');
+xlabel('Iteraciones de QR','FontSize',14.0);
+ylabel('Suma valores bajo la diagonal','FontSize',14.0);
+filename=sprintf('../informe/plots/SUM');
+saveas(h,filename,'png')
+saveas(h,filename,'fig')
+
+
+
+h=figure;
+semilogy([PROM(:,1), PROM(:,2), PROM(:,3)]);
+title('Promedio elementos bajo la diagonal, en funcion de las iteraciones','FontSize',14.0);
+legend('Promedio 5k imagenes','Promedio 15k imagenes','Promedio 30k imagenes','Location','NorthEast');
+xlabel('Iteraciones de QR','FontSize',14.0);
+ylabel('Promedio valores bajo la diagonal','FontSize',14.0);
+filename=sprintf('../informe/plots/PROM');
 saveas(h,filename,'png')
 saveas(h,filename,'fig')
